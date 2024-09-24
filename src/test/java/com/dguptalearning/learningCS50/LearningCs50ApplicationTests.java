8b017e8bb147b4b4c3daaae34165d6bccf6a7114
dguptalearning <136155222+dguptalearning@users.noreply.github.com>
2024-09-24 13:37:16 +0530

    fresh commit

---
+++ src/test/java/com/dguptalearning/learningCS50/LearningCs50ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dguptalearning.learningCS50;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCs50ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
